1236fc2b69bdf8261a07c3b73f5e36b3795c0919
papercd <hpptknk10@hotmail.co.kr>
2024-06-04 08:23:45 +0900

    fixed cfg once again, finished error reporting and parse tree constuction

--- new_main.py
+++ new_main.py
@@ -1,80 +1,61 @@
-
-
 import sys 
 import csv 
 
+
+# Define grammar
 new_grammar = {
-    0: ("CODE", ["VDECL", "CODE"]),
-    1: ("CODE", ["FDECL", "CODE"]),
-    2: ("CODE", [""]),
-    
-    3: ("VDECL", ["vtype", "id", "semi"]),
-    4: ("VDECL", ["vtype", "ASSIGN", "semi"]),
-    
-    5: ("ASSIGN", ["id", "assign", "RHS"]),
-    
-    6: ("RHS", ["EXPR"]),
-    7: ("RHS", ["literal"]),
-    8: ("RHS", ["character"]),
-    9: ("RHS", ["boolstr"]),
-    
-    10: ("EXPR", ["EXPR", "addsub", "TERM"]),
-    11: ("EXPR", ["TERM"]),
-    
-    12: ("TERM", ["TERM", "multdiv", "FACTOR"]),
-    13: ("TERM", ["FACTOR"]),
-    
-    14: ("FACTOR", ["lparen", "EXPR", "rparen"]),
-    15: ("FACTOR", ["id"]),
-    16: ("FACTOR", ["num"]),
-    
-    17: ("FDECL", ["vtype", "id", "lparen", "ARG", "rparen", "lbrace", "BLOCK", "RETURN", "rbrace"]),
-    
-    18: ("ARG", ["vtype", "id", "MOREARGS"]),
-    19: ("ARG", [""]),
-    
-    20: ("MOREARGS", ["comma", "vtype", "id", "MOREARGS"]),
-    21: ("MOREARGS", [""]),
-    
-    22: ("STMT", ["VDECL"]),
-    23: ("STMT", ["ASSIGN", "semi"]),
-    24: ("STMT", ["IF_STMT"]),
-    
-    25: ("IF_STMT", ["if", "lparen", "COND", "rparen", "lbrace", "BLOCK", "rbrace", "ELSE"]),
-    
-    26: ("ELSE", ["else", "lbrace", "BLOCK", "rbrace"]),
-    27: ("ELSE", [""]),
-    
-    28: ("BLOCK", ["STMT", "BLOCK"]),
-    29: ("BLOCK", [""]),
-    
-    30: ("COND", ["boolstr", "COND'"]),
-    
-    31: ("COND'", ["comp", "boolstr", "COND'"]),
-    32: ("COND'", [""]),
-    
-    33: ("RETURN", ["return", "RHS", "semi"]),
+    0: ("S'", ["CODE"]),
+    1: ("CODE", ["VDECL", "CODE"]),
+    2: ("CODE", ["FDECL", "CODE"]),
+    3: ("CODE", [""]),
+    4: ("VDECL", ["vtype", "id", "semi"]),
+    5: ("VDECL", ["vtype", "ASSIGN", "semi"]),
+    6: ("ASSIGN", ["id", "assign", "RHS"]),
+    7: ("RHS", ["EXPR"]),
+    8: ("RHS", ["literal"]),
+    9: ("RHS", ["character"]),
+    10: ("RHS", ["boolstr"]),
+    11: ("EXPR", ["T_EXPR", "addsub", "EXPR"]),
+    12: ("EXPR", ["T_EXPR"]),
+    13: ("T_EXPR", ["F_EXPR", "multdiv", "T_EXPR"]),
+    14: ("T_EXPR", ["F_EXPR"]),
+    15: ("F_EXPR", ["lparen", "EXPR", "rparen"]),
+    16: ("F_EXPR", ["id"]),
+    17: ("F_EXPR", ["num"]),
+    18: ("FDECL", ["vtype", "id", "lparen", "ARG", "rparen", "lbrace", "BLOCK", "RETURN", "rbrace"]),
+    19: ("ARG", ["vtype", "id", "MOREARGS"]),
+    20: ("ARG", [""]),
+    21: ("MOREARGS", ["comma", "vtype", "id", "MOREARGS"]),
+    22: ("MOREARGS", [""]),
+    23: ("BLOCK", ["STMT", "BLOCK"]),
+    24: ("BLOCK", [""]),
+    25: ("STMT", ["VDECL"]),
+    26: ("STMT", ["ASSIGN", "semi"]),
+    27: ("STMT", ["if", "lparen", "COND", "rparen", "lbrace", "BLOCK", "rbrace", "ELSE"]),
+    28: ("STMT", ["while", "lparen", "COND", "rparen", "lbrace", "BLOCK", "rbrace"]),
+    29: ("COND", ["boolstr", "COND_T"]),
+    30: ("COND_T", ["comp", "boolstr", "COND_T"]),
+    31: ("COND_T", [""]),
+    32: ("ELSE", ["else", "lbrace", "BLOCK", "rbrace"]),
+    33: ("ELSE", [""]),
+    34: ("RETURN", ["return", "RHS", "semi"])
 }
 
 
-action_index = 0 
+# Initialize SLR table
+generated_slr_table = {
+    'action': {},
+    'goto': {}
+}
+
+terminal_n_non_terminal = []
+action_index = 0
 goto_index = 0
 
-generated_slr_table = {
-    'action' : {},
-    'goto' : {}
-}
-
-table_entry_dict= {}
-terminal_n_non_terminal = []
-
-#slr table 생성 
-
-with open('new_parsing_table.csv', 'rt', encoding='UTF8') as file:
+# Read the parsing table from CSV
+with open('parsing table.csv', 'rt', encoding='UTF8') as file:
     csvFile = csv.reader(file)
     for i, line in enumerate(csvFile):
-       
-       #무슨 non-terminal과 terminal이 있는지 저장. 
         if i == 0:
             goto_index = len(line) - 1
             for j, entry in enumerate(line):
@@ -83,33 +64,15 @@ with open('new_parsing_table.csv', 'rt', encoding='UTF8') as file:
                         action_index = j
                     terminal_n_non_terminal.append(entry)
         else:
-            #나머지 줄을 읽으면서 dictionary 채우기. 
             state = i - 1
             generated_slr_table['action'][state] = {}
             generated_slr_table['goto'][state] = {}
             for j, entry in enumerate(line):
                 if j != 0 and entry != "":
-                    # action
                     if j <= action_index:
                         generated_slr_table['action'][state][terminal_n_non_terminal[j-1]] = entry
-                    # goto
                     else:
                         generated_slr_table['goto'][state][terminal_n_non_terminal[j-1]] = int(entry)
-
-
-def print_parse_tree(root_node, indent=0):
-    if root_node is None:
-        return
-
-    # Print the current node with appropriate indentation
-   
-    print("  " * indent + (root_node.node_type if root_node.node_type != None else " ") )
-
-    # Recursively print children with increased indentation
-    for child in root_node.children:
-    
-        print_parse_tree(child, indent + 1)
-
 
 class ParseTreeNode:
     def __init__(self, node_type, value=None):
@@ -117,14 +80,40 @@ class ParseTreeNode:
         self.value = value
         self.children = []
 
-#인풋 스트링을 읽는 함수.
+def print_parse_tree(root_node, indent= [0], is_last_child=False):
+    if root_node is None:
+        return
+
+    
+    for el in indent: 
+        if el == 0:
+            print("    " , end="")    
+        else:
+            print("│   " , end="")
+
+    if is_last_child:
+        print("└── ", end="")
+       
+    else:
+        print("├── ", end="")
+    print(root_node.node_type)
+
+    
+    num_children = len(root_node.children) 
+    for i, child in enumerate(root_node.children):
+        is_last = i == num_children - 1
+        if not is_last_child:
+            indent.append(1)
+        else:
+            indent.append(0)
+        print_parse_tree(child, indent , is_last)
+        indent.pop()
 
 
 def read_input_file(file_path):
     try: 
         with open(file_path, 'r', encoding='utf-8') as file:
-            tokens = file.read().split() # 띄어쓰기 단위로 토큰 분리
-            
+            tokens = file.read().split()
             return tokens
     except FileNotFoundError:
         print(f"{file_path} 파일을 찾지 못했습니다.")
@@ -132,11 +121,11 @@ def read_input_file(file_path):
         print(f"오류가 발생했습니다:{e}")
 
 def print_stack(stack):
-    print("stack: [",end="")
+    print("stack: [", end="")
     length = len(stack)
-    for i,element in enumerate(stack): 
-        print( element[1].node_type,",", element[0],end= '' if i == length-1 else ',')
-    print("]")    
+    for i, element in enumerate(stack): 
+        print(element[1].node_type, ",", element[0], end= '' if i == length-1 else ',')
+    print("]")
 
 def parse_with_error_reporting(tokens):
     stack = [(0, ParseTreeNode("CODE"))]  # Stack contains state and parse tree node
@@ -174,11 +163,11 @@ def parse_with_error_reporting(tokens):
             new_tree_node = ParseTreeNode(lhs)
             for _ in rhs:
                 if _ != "":
-                    #stack.pop()  # Pop states and tree nodes for the right-hand side symbols
+                    
                     _, child_node = stack.pop()
-                    new_tree_node.children.insert(0, child_node)  # Insert children in reverse order
+                    new_tree_node.children.insert(0, child_node)
                    
-            # Consult the goto table to find the next state
+          
             
             next_state = generated_slr_table['goto'][stack[-1][0]][lhs]
             stack.append((next_state, new_tree_node))
@@ -207,19 +196,14 @@ tokens = read_input_file("test.txt")
 
 parse_tree_root, error_report = parse_with_error_reporting(tokens)
 if error_report:
-    print()
-    print("error occured-----------------------")
+    print("\nError occurred -----------------------")
     print("Parsing error:")
     print(error_report['message'])
     print("Token:", error_report['token'])
     print("Expected tokens:", error_report['expected_tokens'])
     print("Context:", error_report['context'])
 else:
-    print("done")
-    print()
-    print("parse tree---------------------------------")
-    print()
-    print_parse_tree(parse_tree_root)
-    # Continue with successful parsing
+    print("-------------------------- Parse Tree ---------------------------------")
+    print("    S'")
 
-
+    print_parse_tree(parse_tree_root,is_last_child=True)
